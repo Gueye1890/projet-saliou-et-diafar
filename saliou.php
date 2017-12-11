20fb646a04522fdd42ac8765a96841cf20eb7812
Gueye1890 <34451380+Gueye1890@users.noreply.github.com>
2017-12-11 15:51:59 +0000

    Add files via upload

---
+++ saliou.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Formulaire</title>
+</head>
+<body style="text-align: center;">
+	<h1>Bienvenue</h1>
+	<form method="POST" action="resultat.php">	
+		<input type="text" name="nom" placeholder="Entrer votre nom" required><br><br>
+		<input type="text" name="age" placeholder="Entrer votre age" required><br><br>
+		<input type="submit" name="submit" value="Valider">
+	</form>
+</body> 
+</html>
